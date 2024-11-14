2a7c80c579e3f77891ce9010b240b43b3f643b51
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-11-14 23:27:29 +0900

    fix: eslint, prettierを修正

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,25 +1,25 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.use(async (_, next) => {
   console.log(`request`);
   await next();
-})
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/ping', (c) => {
-  return c.json({'message': `hi your message is ${c.req.query('message')}`})
 });
 
-const port = 3001
-console.log(`Server is running on http://localhost:${port}`)
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.get('/ping', (c) => {
+  return c.json({ message: `hi your message is ${c.req.query('message')}` });
+});
+
+const port = 3001;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
