ba1c4f7ccc0647e52c6c186d39ba91603449c2f3
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-11-29 23:49:12 +0900

    add: OpenAPIHonoの追加

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,8 @@
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { swaggerUI } from '@hono/swagger-ui';
+import { OpenAPIHono, z } from '@hono/zod-openapi';
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
 app.use(async (_, next) => {
   console.log(`request`);
@@ -15,6 +16,16 @@ app.get('/', (c) => {
 app.get('/ping', (c) => {
   return c.json({ message: `hi your message is ${c.req.query('message')}` });
 });
+
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: 'α0.1.0',
+    title: 'Auction API',
+  },
+});
+
+app.get('/doc/ui', swaggerUI({ url: '/doc' }));
 
 const port = 3001;
 console.log(`Server is running on http://localhost:${port}`);
