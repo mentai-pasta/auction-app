9dc94c90bd00475920e32fe57b421fa16f049bba
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-11-30 19:14:57 +0900

    fix: 使用引数の変更

--- deploy-pull.sh
+++ deploy-pull.sh
@@ -4,5 +4,5 @@ if [ "$1" = "" ] && [ "$2" = "" ]
 then
     echo "no argument"
 else
-    ssh $0@$1 "cd /home/github/apps/hal-platform/docker-files/node/projects/auction-app && git pull origin" | echo
+    ssh $1@$2 "cd /home/github/apps/hal-platform/docker-files/node/projects/auction-app && git pull origin" | echo
 fi
