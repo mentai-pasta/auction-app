66cf2fb8cc48c433200128a7a5c23f99292701b5
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-12-02 23:22:19 +0900

    fix: configに設定を書き込むよう変更

--- deploy-pull.sh
+++ deploy-pull.sh
@@ -4,5 +4,5 @@ if [ "$1" = "" ] && [ "$2" = "" ] && [ "$3" = "" ]
 then
     echo "no argument"
 else
-    ssh $1@$2 -o StrictHostKeyChecking=no -o ProxyCommand="$3" "cd /home/github/apps/hal-platform/docker-files/node/projects/auction-app && git pull origin" | echo
+    ssh dev-server "cd /home/github/apps/hal-platform/docker-files/node/projects/auction-app && git pull origin" | echo
 fi
--- deploy-restart.sh
+++ deploy-restart.sh
@@ -4,5 +4,5 @@ if [ "$1" = "" ] && [ "$2" = "" ]
 then
     echo "no argument"
 else
-    ssh $1@$2 -o StrictHostKeyChecking=no -o ProxyCommand="$SSH_PROXY_COMMAND" "cd /home/github/apps/hal-platform && docker compose restart" | echo
+    ssh dev-server "cd /home/github/apps/hal-platform && docker compose restart" | echo
 fi
