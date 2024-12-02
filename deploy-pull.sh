ccd5146448566a78dbb6960474caa010ee56b0bf
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-12-02 23:23:49 +0900

    fix: 引数チェックを削除

--- deploy-pull.sh
+++ deploy-pull.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ "$1" = "" ] && [ "$2" = "" ] && [ "$3" = "" ]
-then
-    echo "no argument"
-else
-    ssh dev-server "cd /home/github/apps/hal-platform/docker-files/node/projects/auction-app && git pull origin" | echo
-fi
+ssh dev-server "cd /home/github/apps/hal-platform/docker-files/node/projects/auction-app && git pull origin" | echo
--- deploy-restart.sh
+++ deploy-restart.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ "$1" = "" ] && [ "$2" = "" ]
-then
-    echo "no argument"
-else
-    ssh dev-server "cd /home/github/apps/hal-platform && docker compose restart" | echo
-fi
+ssh dev-server "cd /home/github/apps/hal-platform && docker compose restart" | echo
