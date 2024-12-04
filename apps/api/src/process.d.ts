c493b61c6db2bd5f5e8051ac15fc2a354d5668ac
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-12-04 22:09:44 +0900

    add: envを追加

--- apps/api/src/process.d.ts
+++ apps/api/src/process.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly DATABASE_URL: string;
     readonly npm_package_version: string;
+    readonly R2_ACCOUNT_ID: string;
+    readonly R2_ACCESS_KEY_ID: string;
+    readonly R2_SECRET_ACCESS_KEY: string;
+    readonly R2_BUCKET_NAME: string;
   }
 }
