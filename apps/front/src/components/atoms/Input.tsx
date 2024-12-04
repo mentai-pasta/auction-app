57b50e66885cf91decf3ce3a777b8c7bd369330e
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-12-04 23:10:15 +0900

    add: InputComponent

---
+++ apps/front/src/components/atoms/Input.tsx
@@ -0,0 +1,49 @@
+import React, { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+    inputSize?: 'input-xs' | 'input-sm' | 'input-md' | 'input-lg';
+    variant?: 'input-bordered' | 'input-ghost' | 'input-primary' | 'input-secondary' | 'input-accent' | 'input-info' | 'input-success' | 'input-warning' | 'input-error';
+};
+
+/**
+ * daisyUI使用のInput
+ * 
+ * @example
+ * export default function Home() {
+ *     return (
+ *         <>
+ *             <Input 
+ *                 variant="input-bordered" 
+ *                 inputSize="input-md" 
+ *                 label="E-mail" 
+ *                 placeholder="emailを入力"/>
+ *         </>
+ *     );
+ * }
+ */
+export const Input: React.FC<InputProps> = ({ 
+    label, 
+    variant = 'input-bordered', 
+    inputSize = 'input-md', 
+    ...input 
+}) => {
+    // ラベル指定のない場合はラベル要素ごと消す
+    const Label: React.FC = () => {
+        if (label) {
+            return (
+                <div className="label">
+                    <div className="label-text">{label}</div>
+                </div>
+            );
+        }
+        return (<></>);
+    }
+
+    return (
+        <label htmlFor="form-control w-full">
+            <Label/>
+            <input className={`input ${variant} ${inputSize}`} {...input}/>
+        </label>
+    );
+}
