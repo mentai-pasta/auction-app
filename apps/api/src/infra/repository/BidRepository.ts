a0f5892c52b66af7f14953eeef015429e309b904
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-12-07 14:19:29 +0900

    add: 入札用レポジトリの追加

---
+++ apps/api/src/infra/repository/BidRepository.ts
@@ -0,0 +1,52 @@
+import { z } from '@hono/zod-openapi';
+import { eq, max } from 'drizzle-orm';
+import { PostBidBodySchema } from '../../application/schemas/BidSchema.js';
+import { bids } from '../entity/schema.js';
+import { db } from '../helper/db.js';
+type PostBidBodySchema = z.infer<typeof PostBidBodySchema>;
+
+export class BidRepository {
+  // 新規入札
+  async createBid(body: PostBidBodySchema) {
+    const { customer_id, stock_id, price } = body;
+
+    return await db.transaction(async (trx) => {
+      const result = await trx
+        .select({ value: max(bids.price) })
+        .from(bids)
+        .where(eq(bids.stockId, stock_id));
+      const max_bid = result[0];
+
+      if (max_bid.value !== null && max_bid.value >= price.toString()) {
+        throw new Error('low bid price');
+      } else {
+        const result = await trx
+          .insert(bids)
+          .values({
+            stockId: stock_id,
+            customerId: customer_id,
+            price: price.toString(),
+          })
+          .returning({ insertId: bids.bidId });
+
+        return {
+          bid_id: result[0].insertId,
+          price: price,
+        };
+      }
+    });
+  }
+
+  // 最高入札額取得
+  async getMaxBidPrice(stock_id: string) {
+    return await db.transaction(async (trx) => {
+      const result = await trx
+        .select({ value: max(bids.price) })
+        .from(bids)
+        .where(eq(bids.stockId, stock_id));
+      const max_bid = result[0].value;
+      console.log('max_bid:', max_bid);
+      return max_bid;
+    });
+  }
+}
