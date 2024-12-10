c07c24149947e37377737ac1705d97ac0e0ba33b
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-12-10 17:04:43 +0000

    fix: alert component
    flexレイアウトに変更

--- apps/front/src/components/atoms/Alert.tsx
+++ apps/front/src/components/atoms/Alert.tsx
@@ -1,54 +1,65 @@
-import React, { InputHTMLAttributes } from 'react';
+import React from 'react';
 
 interface AlertProps {
-    alertType?: 'alert' | 'alert-success' | 'alert-error' | 'alert-warning' | 'alert-info';
-    label?: string;
-    // daisyUIのbtnプロパティ
-    btnType?: 'btn-success' | 'btn-error' | 'btn-warning' | 'btn-info';
-    btnLabel?: string;
-    btnDisplay?: boolean;
-    onClick?: () => void;
-};
+  alertType?: 'alert' | 'alert-success' | 'alert-error' | 'alert-warning' | 'alert-info';
+  children?: React.ReactNode;
+  // daisyUIのbtnプロパティ
+  btnType?: 'btn-success' | 'btn-error' | 'btn-warning' | 'btn-info';
+  btnLabel?: string;
+  btnDisplay?: boolean;
+  onClick?: () => void;
+}
 
 /**
  * daisyUI使用のAlert
- * 
+ *
+ *
  * @example
  * export default function Home() {
  *     return (
- *             <Alert  type='alert-error' label='testText' btnType="btn-success" btnLabel="testButton" btnDisplay={false}> 
+ *             <Alert type='alert-error' btnType="btn-success" btnLabel="testButton" btnDisplay>
+ *                 <p>テストアラート</p>
+ *             </Alert>
  *     );
  * }
  */
 
 export const Alert: React.FC<AlertProps> = ({
-    alertType,
-    label,
-    btnType,
-    btnLabel,
-    btnDisplay = false,
-    onClick,
+  alertType,
+  children,
+  btnType = 'btn-info',
+  btnLabel = 'Button',
+  btnDisplay = false,
+  onClick,
 }) => {
-    let btnHide = '';
-    btnHide = btnDisplay ? 'block' : 'hidden';
-
-    return (
-        <div role='alert' className={`alert ${alertType}`}>
-            <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6 shrink-0 stroke-current'
-                fill='none'
-                viewBox='0 0 24 24'>
-                <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z' />
-            </svg>
-            <span>{label}</span>
-            <div>
-                <button className={`btn ${btnType} ${btnHide}`} onClick={onClick}> {btnLabel}</button>
-            </div>
-        </div >
+  /** ボタン表示がない場合はボタン要素ごと消す */
+  const AlertButton = () => {
+    return btnDisplay ? (
+      <button className={`btn ${btnType}`} onClick={onClick}>
+        {btnLabel}
+      </button>
+    ) : (
+      <></>
     );
-}
+  };
+
+  return (
+    <div role="alert" className={`alert ${alertType} flex items-center`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="stroke-neutral h-6 w-6 shrink-0"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <div className="text-left">{children}</div>
+      <AlertButton />
+    </div>
+  );
+};
