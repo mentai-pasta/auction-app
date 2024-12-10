efe2f93a98809ddd4c5dbb3913d77ec87c9d9fc0
96nekocake <96nekocake@gmail.com>
2024-12-10 11:03:21 +0000

    update:apiクライアントurlを変更
    'credentials: include' を追加

--- apps/front/src/utils/useAuth.ts
+++ apps/front/src/utils/useAuth.ts
@@ -1,13 +1,15 @@
 import { hc } from 'hono/client';
 import { ApiType } from 'api/src/index';
 
-const client = hc<ApiType>('/api/v1');
+const client = hc<ApiType>('http://localhost:3001/api/v1');
 
 export const useAuth = async () => {
-    const rawRes = await client.api.v1.verify.$get();
+    const rawRes = await client.verify.$get({
+        credentials: 'include', 
+    });
 
     let res = null;
-    if (rawRes.ok) res = rawRes.json();
+    if (rawRes.ok)  res = await rawRes.json();
 
     return res;
 };
