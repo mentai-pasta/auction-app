07b42352e22549e47dfc8a4136028aeebb974c97
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-12-10 14:21:31 +0900

    add: ログイン用Tokenの払出処理を分割

--- apps/api/src/infra/helper/jwt.ts
+++ apps/api/src/infra/helper/jwt.ts
@@ -1,7 +1,24 @@
 import 'dotenv/config';
+import { sign } from 'hono/jwt';
 
 if (!process.env.JWT_SECRET) {
   throw new Error('JWT_SECRET is not defined');
 }
 
 export const secret = process.env.JWT_SECRET;
+
+export const loginToken = async (
+  customer_id: string,
+  customer_name: string,
+  email: string,
+): Promise<string> => {
+  return await sign(
+    {
+      customer_id: customer_id,
+      customer_name: customer_name,
+      email: email,
+      exp: Math.floor(Date.now() / 1000) + 60 * 1, // 1 minute
+    },
+    secret,
+  );
+};
