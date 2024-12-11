4b764bc5c5efbe87093c9502c687db80498b0120
96nekocake <96nekocake@gmail.com>
2024-12-11 08:10:16 +0000

    update::token取得の処理を追加

--- apps/front/src/utils/useAuth.ts
+++ apps/front/src/utils/useAuth.ts
@@ -1,15 +1,20 @@
+'use server'
+import { cookies } from 'next/headers'
+
 import { hc } from 'hono/client';
 import { ApiType } from 'api/src/index';
 
-const client = hc<ApiType>('http://localhost:3001/api/v1');
+const client = hc<ApiType>('http://localhost:3001/');
 
 export const useAuth = async () => {
-    const rawRes = await client.verify.$get({
-        credentials: 'include', 
+    const cookieStore = await cookies();
+    const jwtToken = cookieStore.get('token');
+    const rawRes = await client.api.v1.verify.$get({
+        token: jwtToken,
     });
 
     let res = null;
-    if (rawRes.ok)  res = await rawRes.json();
+    if (rawRes.ok) res = await rawRes.json();
 
     return res;
 };
