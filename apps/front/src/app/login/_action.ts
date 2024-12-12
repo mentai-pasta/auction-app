3870e38fb3026989c94d78df06effc5df7eac3d8
96nekocake <96nekocake@gmail.com>
2024-12-12 17:12:43 +0000

    update:取得元、送信先を修正

--- apps/front/src/app/login/_action.ts
+++ apps/front/src/app/login/_action.ts
@@ -3,11 +3,16 @@ import { hc } from "hono/client";
 
 const client = hc<ApiType>("http://localhost:3001/");
 
-export const sendBid = (form: FormData) => {
-    form.get('bid-select'); // name="bid-select"のデータを取得
-    client.api.v1.login.$post({
+export const sendLogin = async (form: FormData) => {
+    const rawRes = await client.api.v1.login.$post({
         json: {
-            customer_id: `fdd25989-085e-fc11-31f0-a4a25095a47d`,
+            email: form.get("loginId"),
+            password: form.get("password"),
         }
     });
+
+    let res = null;
+    if (rawRes.ok) res = await rawRes.json();
+
+    return res;
 };
