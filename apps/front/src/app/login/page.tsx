1a9ef9a10e0dae02c8eab44abd3d469ea0b09b86
96nekocake <96nekocake@gmail.com>
2024-12-12 16:15:00 +0000

    add:loginPage追加（制作途中）

---
+++ apps/front/src/app/login/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { Button } from '@/components/atoms/Button';
+import { Input } from '@/components/atoms/Input';
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+    return (
+        <>
+            <main className="flex items-center justify-center min-h-screen bg-gray-100">
+                <div className="bg-neutral text-white p-8 rounded-lg shadow-lg w-480">
+                    <h2 className="text-center text-xl font-semibold mb-4">ログイン</h2>
+                    <form className="flex flex-col space-y-4">
+                        <Input label="ログインID" placeholder="ログインID" />
+                        <Input label="パスワード" placeholder="パスワード" />
+                        <Button btnType="btn-primary" label="ログイン" />
+                    </form>
+                    <p className="text-center text-sm mt-4">
+                        アカウントをお持ちでない方はこちら
+                    </p>
+                </div>
+            </main>
+        </>
+    );
+}
